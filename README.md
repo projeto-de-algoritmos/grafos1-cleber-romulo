9b0cb48dcb684f3d4bd5e6d9a5fe898715a074f5
Cleber Junior <cjj.castro3@gmail.com>
2019-04-04 22:36:07 -0300

    Created readme, added a difrent layout

---
+++ README.md
@@ -0,0 +1,23 @@
+# Busca em largura
+
+O projeto desenvolvido aplica busca em largura, para, de forma visual, simular o funcionamento do algoritmo.
+
+## Autores
+
+| Nomde | github |
+|:-----:|:------:|
+| Cleber Júnior | [@cjjcastro](https://github.com/cjjcastro/) |
+| Rômulo Vinícius de Souza | [@cjjcastro](https://github.com/romulosouza/) |
+
+
+## Instalação e Execução
+
+
+```shell
+$ git clone https://github.com/projeto-de-algoritmos/grafos1-cleber-romulo.git
+$ npm install
+$ npm start
+```
+obs: Para executar a aplicação é necessário a instalação do [nodejs](https://nodejs.org/en/), a versão utilizada foi a 10.15.3.
+
+![aplicacao](assets/img/aplicacao2.gif)
--- graph.js
+++ graph.js
@@ -1,66 +1,6 @@
 var color = 'gray';
 var len = undefined;
-// var nodes = [{ id: 0, label: "0" },
-// { id: 1, label: "1", color: 'black' },
-// { id: 2, label: "2" },
-// { id: 3, label: "3" },
-// { id: 4, label: "4" },
-// { id: 5, label: "5" },
-// { id: 6, label: "6" },
-// { id: 7, label: "7" },
-// { id: 8, label: "8" },
-// { id: 9, label: "9" },
-// { id: 10, label: "10" },
-// { id: 11, label: "11" },
-// { id: 12, label: "12" },
-// { id: 13, label: "13" },
-// { id: 14, label: "14" },
-// { id: 15, label: "15" },
-// { id: 16, label: "16" },
-// { id: 17, label: "17" },
-// { id: 18, label: "18" },
-// { id: 19, label: "19" },
-// { id: 20, label: "20" },
-// { id: 21, label: "21" },
-// { id: 22, label: "22" },
-// { id: 23, label: "23" },
-// { id: 24, label: "24" },
-// { id: 25, label: "25" },
-// { id: 26, label: "26" },
-// { id: 27, label: "27" },
-// { id: 28, label: "28" },
-// { id: 29, label: "29" }
-// ];
-// var edges = [{ from: 1, to: 0 },
-// { from: 2, to: 0 },
-// { from: 4, to: 3 },
-// { from: 5, to: 4 },
-// { from: 4, to: 0 },
-// { from: 7, to: 6 },
-// { from: 8, to: 7 },
-// { from: 7, to: 0 },
-// { from: 10, to: 9 },
-// { from: 11, to: 10 },
-// { from: 10, to: 4 },
-// { from: 13, to: 12 },
-// { from: 14, to: 13 },
-// { from: 13, to: 0 },
-// { from: 16, to: 15 },
-// { from: 17, to: 15 },
-// { from: 15, to: 10 },
-// { from: 19, to: 18 },
-// { from: 20, to: 19 },
-// { from: 19, to: 4 },
-// { from: 22, to: 21 },
-// { from: 23, to: 22 },
-// { from: 22, to: 13 },
-// { from: 25, to: 24 },
-// { from: 26, to: 25 },
-// { from: 25, to: 7 },
-// { from: 28, to: 27 },
-// { from: 29, to: 28 },
-// { from: 28, to: 0 }
-// ]
+
 nodes = [];
 edges = [];
 var connectionCount = [];
@@ -128,7 +68,7 @@ var options = {
     manipulation: {
         enabled: true,
         addNode: function (nodeData, callback) {
-            nodeData.label = nodeData.id
+            nodeData.label = ""
             callback(nodeData);
         },
         addEdge: function (edgeData, callback) {
@@ -203,7 +143,7 @@ function bfs(value) {
         scale: 1,
         // animation: true
         animation: {
-            duration: 1800,
+            duration: 800,
             // easingFunction: linear
         }
     }
